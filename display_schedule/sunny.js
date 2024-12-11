9bf71e6c5791c53b1cfe78e03ba1042b7b9e4cb9
sakuramotida <it232089@trident.ac.jp>
2024-12-11 11:29:33 +0900

    フォーマット調整

--- display_schedule/sunny.js
+++ display_schedule/sunny.js
@@ -5,19 +5,19 @@ fetch('sample.json')
     let output = ''; // 表示する文字列を格納する変数
 
     for (let a = 0; a < data.days.length; a++) {
-        if(a % 2 == 1){
-          console.log('a');
-          const day = data.days[a].date;
-          output += `<h2 class ='day'>${day}</h2>`
-        }
+      if (a % 2 == 1) {
+        console.log('a');
+        const day = data.days[a].date;
+        output += `<h2 class ='day'>${day}</h2>`
+      }
       for (let i = 0; i < data.days[a].schedule.length; i++) {
-        if(a % 2 == 1){
-        const b = data.days[a].schedule[i].activity;
-        const c = data.days[a].schedule[i].time;
-        output += `<h3>${c}</h3><p class='p'>${b}</p><br>`; // 各アクティビティを`<p>`タグで囲んで出力
-        }  
+        if (a % 2 == 1) {
+          const b = data.days[a].schedule[i].activity;
+          const c = data.days[a].schedule[i].time;
+          output += `<h3>${c}</h3><p class='p'>${b}</p><br>`; // 各アクティビティを`<p>`タグで囲んで出力
+        }
+      }
     }
-}
     console.log(output);
     resultElement.innerHTML = output; // 結果をHTMLに表示
   });
