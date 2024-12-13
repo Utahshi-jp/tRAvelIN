97805687f35922ede6c1c8343e85c47a25276613
sakuramotida <it232089@trident.ac.jp>
2024-12-13 11:48:07 +0900

    表示形式変更

--- display_schedule/sunny.js
+++ display_schedule/sunny.js
@@ -1,20 +1,43 @@
-fetch('sample.json')//jsonコード呼び出し
+fetch('sample.json')
   .then(response => response.json())
   .then(data => {
     const resultElement = document.getElementById('sunny');
+    const confirmedbutton = document.getElementById('button');
     let output = ''; // 表示する文字列を格納する変数
+    let output2 = '';
+    let yotei = '';
 
-    // jsonの値取得の処理
     for (let a = 0; a < data.days.length; a++) {
       if (a % 2 == 0) {
+        // console.log('a');
         const day = data.days[a].date;
-        output += `<h2 class ='day'>${day}</h2>`//日付の取得
+        output += `<h2 class ='day'>${day}</h2>`
       }
-      for (let i = 0; i < data.days[a].schedule.length; i++) {//旅行の時間(1日の旅行予定)があるだけfor文で取得
-        if (a % 2 === 0) {//晴れであるかの判定(jsonは交互に晴れ・雨で入っているため奇数であるほうが晴晴れ)
-          const b = data.days[a].schedule[i].activity;//時間の取得
-          const c = data.days[a].schedule[i].time;;//旅行予定の取得
-          output += `<h3 class = 'h3'>${c}</h3><input type="text" value="${b}" class='p'><br>`; // 各アクティビティを`<p>`タグで囲んで出力
+      for (let i = 0; i < data.days[a].schedule.length; i++) {
+        if (a % 2 == 0) {
+          const b = data.days[a].schedule[i].activity;
+          const c = data.days[a].schedule[i].time;
+          output += `<p class = 'sunnytime'>${c}</p><input id =${i} name = "trip" type="text" value="${b}" class='p'><br>`;  // 各アクティビティを`<p>`タグで囲んで出力
+          // console.log(b);
+
+          confirmedbutton.addEventListener('click', () => {
+            //クリックイベント
+            let output = '';
+            for (var atai = 0; atai < data.days[a].schedule.length; atai++) {
+              inputElm = document.getElementById(atai);
+              console.log(inputElm);
+              yotei = inputElm.value;
+            
+            if (a % 2 == 0) {
+              // console.log(a % 2 == 0);
+              const day = data.days[a].date;
+              output2 += `<h2 class ='day'>${day}</h2>`
+            }
+            output2 += `<p class = 'sunnytime'>${c}</p><p class='p'>${yotei}</p><br>`;
+            // console.log(output);
+            resultElement.innerHTML = output2;
+          }
+          });
         }
       }
     }
