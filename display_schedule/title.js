e2997e4849ad6aed6177b62ff96048240c16c707
sakuramotida <it232089@trident.ac.jp>
2024-12-16 14:52:04 +0900

    プログラムを添削

--- display_schedule/title.js
+++ display_schedule/title.js
@@ -4,8 +4,8 @@ fetch('sample.json')
     const resultElement = document.getElementById('title');
     let output = ''; // 表示する文字列を格納する変数
     console.log(data);
-        const b = data.title;
-        output += `<h1>${b}</h1>`; // 各アクティビティを`<p>`タグで囲んで出力
+    const b = data.title;
+    output += `<h1>${b}</h1>`; // 各アクティビティを`<p>`タグで囲んで出力
     console.log(output);
     resultElement.innerHTML = output; // 結果をHTMLに表示
   });
