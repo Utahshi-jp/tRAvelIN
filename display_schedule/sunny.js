6c451d9809c75c154dbd2229bd6bf5ec7af1339e
sakuramotida <it232089@trident.ac.jp>
2024-12-16 13:48:28 +0900

    調整コミット

--- display_schedule/sunny.js
+++ display_schedule/sunny.js
@@ -6,7 +6,7 @@ fetch('sample.json')
     let output = ''; // 表示する文字列を格納する変数
     let output2 = '';
     let yotei = '';
-
+    let atai = 0;
     for (let a = 0; a < data.days.length; a++) {
       if (a % 2 == 0) {
         // console.log('a');
@@ -17,30 +17,11 @@ fetch('sample.json')
         if (a % 2 == 0) {
           const b = data.days[a].schedule[i].activity;
           const c = data.days[a].schedule[i].time;
-          output += `<p class = 'sunnytime'>${c}</p><input id =${i} name = "trip" type="text" value="${b}" class='p'><br>`;  // 各アクティビティを`<p>`タグで囲んで出力
+          output += `<p class = 'sunnytime'>${c}</p><input id =${i} name = "trip" type="trip" value=${b} class='p'><br>`;  // 各アクティビティを`<p>`タグで囲んで出力
           // console.log(b);
-
-          confirmedbutton.addEventListener('click', () => {
-            //クリックイベント
-            let output = '';
-            for (var atai = 0; atai < data.days[a].schedule.length; atai++) {
-              inputElm = document.getElementById(atai);
-              console.log(inputElm);
-              yotei = inputElm.value;
-            
-            if (a % 2 == 0) {
-              // console.log(a % 2 == 0);
-              const day = data.days[a].date;
-              output2 += `<h2 class ='day'>${day}</h2>`
-            }
-            output2 += `<p class = 'sunnytime'>${c}</p><p class='p'>${yotei}</p><br>`;
-            // console.log(output);
-            resultElement.innerHTML = output2;
-          }
-          });
         }
       }
     }
-    console.log(output);
+    // console.log(output);
     resultElement.innerHTML = output; // 結果をHTMLに表示
   });
