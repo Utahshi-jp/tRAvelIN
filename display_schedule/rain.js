775b3bee1fbc0054b9b31e9e0308a129edd290a9
sakuramotida <it232089@trident.ac.jp>
2024-12-17 11:07:39 +0900

    コメント追加

--- display_schedule/rain.js
+++ display_schedule/rain.js
@@ -1,24 +1,24 @@
+// jsonファイルの取り出し
 fetch('sample.json')
   .then(response => response.json())
   .then(data => {
-    const resultElement = document.getElementById('rain');
-    const confirmedbutton = document.getElementById('rainbutton');
+    const resultElement = document.getElementById('rain');//htmlとの連携
+    const confirmedbutton = document.getElementById('rainbutton');//htmlとの連携
     let output = ''; // 表示する文字列を格納する変数
-    let output2 = '';
-    let ran = 0;
+    let ran = 0;//数字がかぶるのを防ぐ(晴れの数字とも被ってはいけない!)
 
-    for (let a = 0; a < data.days.length; a++) {
-      if (a % 2 == 1) {
+    for (let a = 0; a < data.days.length; a++) {//jsonデータがある間
+      if (a % 2 == 1) {//雨の判定
         // console.log(a % 2 == 1);
-        const day = data.days[a].date;
-        output += `<h2 class ='day'>${day}</h2>`
-        ran += 100; 
+        const day = data.days[a].date;//日付を取り出す
+        output += `<h2 class ='day'>${day}</h2>`//日付の表示
+        ran += 100;
       }
-      for (let i = 0; i < data.days[a].schedule.length; i++) {
-        if (a % 2 === 1) {
-          const b = data.days[a].schedule[i].activity;
-          const c = data.days[a].schedule[i].time;
-          let atai = a + i + ran;
+      for (let i = 0; i < data.days[a].schedule.length; i++) {//旅行何日分か？
+        if (a % 2 === 1) {//雨の判定
+          const b = data.days[a].schedule[i].activity;//旅行予定の取り出し
+          const c = data.days[a].schedule[i].time;//時間の取り出し
+          let atai = a + i + ran;//inputに付けるid作成(idが一つでもかぶるとボタン処理をしたときに表示が狂うためran変数を使い調整)
           console.log(atai);
           output += `<p class = 'raintime'>${c}</p><input id = ${atai} type="text" value="${b}" class='input'><br>`; // 各アクティビティを`<p>`タグで囲んで出力
           // 結果をHTMLに表示
@@ -28,30 +28,33 @@ fetch('sample.json')
       }
     }
 
+    // 予定確定ボタン押下時の処理
     confirmedbutton.addEventListener('click', () => {
-      let output = '';
-      let ran = 0;
+      let output = '';//もともと入っているhtml(初期状態)の初期化
+      let output2 = '';//ボタンが2回以上押された際、入っているデータの初期化(上書きのため)
+      let ran = 0;//数字がかぶらないようにする
       console.log(output);
-      for (let a = 0; a < data.days.length; a++) {
-        if (a % 2 == 1) {
+      for (let a = 0; a < data.days.length; a++) {//jsonデータがある間
+        if (a % 2 == 1) {//雨の判定
           // console.log(a % 2 == 1);
-          const day = data.days[a].date;
-          output2 += `<h2 class ='day'>${day}</h2>`
+          const day = data.days[a].date;//日付の取り出し
+          output2 += `<h2 class ='day'>${day}</h2>`//日付の表示
           ran += 100;
         }
-        for (let i = 0; i < data.days[a].schedule.length; i++) {
-          if (a % 2 == 1) {
-            let atai = a + i + ran;
+        for (let i = 0; i < data.days[a].schedule.length; i++) {//旅行何日分か？
+          if (a % 2 == 1) {//雨の判定
+            let atai = a + i + ran;//inputに付けるid作成(idが一つでもかぶるとボタン処理をしたときに表示が狂うためran変数を使い調整)
             console.log(i);
+            // 49,50行目でユーザが変更した内容(全てのinputの中身)を取得(これがないとユーザが変更した場合変更した内容が取れない)
             const inputElm = document.getElementById(atai);
             const inputValue = inputElm.value;
             console.log(inputValue);
-            const c = data.days[a].schedule[i].time;
-            output2 += `<p class = 'raintime'>${c}</p><p class='p'>${inputValue}</p>`; // 各アクティビティを`<p>`タグで囲んで出力
+            const c = data.days[a].schedule[i].time;//時間の取得
+            output2 += `<p class = 'raintime'>${c}</p><input id = ${atai} type="text" value="${inputValue}" class='input'><br>`; // 各アクティビティを`<p>`タグで囲んで出力
           }
         }
       }
+      // 結果をHTMLに表示
       resultElement.innerHTML = output2;
     });
-
   });
--- display_schedule/sunny.js
+++ display_schedule/sunny.js
@@ -1,24 +1,24 @@
+// jsonファイルの取り出し
 fetch('sample.json')
   .then(response => response.json())
   .then(data => {
-    const resultElement = document.getElementById('sunny');
-    const confirmedbutton = document.getElementById('sunnybutton');
+    const resultElement = document.getElementById('sunny');//htmlとの連携
+    const confirmedbutton = document.getElementById('sunnybutton');//htmlとの連携
     let sunnyoutput = ''; // 表示する文字列を格納する変数
-    let sunnyoutput2 = '';
-    let ran = 0;
+    let ran = 0;;//数字がかぶるのを防ぐ(雨の数字とも被ってはいけない!)
 
-    for (let a = 0; a < data.days.length; a++) {
-      if (a % 2 == 0) {
+    for (let a = 0; a < data.days.length; a++) {//jsonデータがある間
+      if (a % 2 == 0) {//晴れの判定
         // console.log(a % 2 == 1);
-        const day = data.days[a].date;
-        sunnyoutput += `<h2 class ='day'>${day}</h2>`
-        ran += 1000;
+        const day = data.days[a].date;//日付を取り出す
+        sunnyoutput += `<h2 class ='day'>${day}</h2>`//日付の表示
+        ran += 1000;//数字がかぶるのを防ぐ(雨の数字とも被ってはいけない!)
       }
       for (let i = 0; i < data.days[a].schedule.length; i++) {
-        if (a % 2 === 0) {
-          const b = data.days[a].schedule[i].activity;
-          const c = data.days[a].schedule[i].time;
-          let sunnyatai = a + i + ran;
+        if (a % 2 === 0) {//晴れの判定
+          const b = data.days[a].schedule[i].activity;//旅行予定の取り出し
+          const c = data.days[a].schedule[i].time;//時間の取り出し
+          let sunnyatai = a + i + ran;//inputに付けるid作成(idが一つでもかぶるとボタン処理をしたときに表示が狂うためran変数を使い調整)
           console.log(sunnyatai);
           sunnyoutput += `<p class = 'sunnytime'>${c}</p><input id = ${sunnyatai} type="text" value="${b}" class='input'><br>`; // 各アクティビティを`<p>`タグで囲んで出力
           // 結果をHTMLに表示
@@ -29,28 +29,32 @@ fetch('sample.json')
     }
 
     confirmedbutton.addEventListener('click', () => {
-      let output = '';
-      let ran = 0;
+      let output = '';//もともと入っているhtml(初期状態)の初期化
+      let sunnyoutput2 = '';//ボタンが2回以上押された際、入っているデータの初期化(上書きのため)
+      let ran = 0;//数字がかぶるのを防ぐ(雨の数字とも被ってはいけない!)
       console.log(output);
-      for (let a = 0; a < data.days.length; a++) {
-        if (a % 2 == 0) {
+      for (let a = 0; a < data.days.length; a++) {//jsonデータがある間
+        if (a % 2 == 0) {//晴れの判定
           // console.log(a % 2 == 1);
-          const day = data.days[a].date;
-          sunnyoutput2 += `<h2 class ='day'>${day}</h2>`
+          const day = data.days[a].date;//日付を取り出す
+          sunnyoutput2 += `<h2 class ='day'>${day}</h2>`//日付の表示
           ran += 1000;
         }
         for (let i = 0; i < data.days[a].schedule.length; i++) {
-          if (a % 2 == 0) {
-            let sunnyatai = a + i + ran;
+          if (a % 2 == 0) {//晴れの判定
+            let sunnyatai = a + i + ran;//数字がかぶるのを防ぐ(雨の数字とも被ってはいけない!)
             console.log(i);
+            // 48,49行目でユーザが変更した内容(全てのinputの中身)を取得(これがないとユーザが変更した場合変更した内容が取れない
             const inputElm = document.getElementById(sunnyatai);
             const inputValue = inputElm.value;
             console.log(inputValue);
-            const c = data.days[a].schedule[i].time;
-            sunnyoutput2 += `<p class = 'sunnytime'>${c}</p><p class='p'>${inputValue}</p>`; // 各アクティビティを`<p>`タグで囲んで出力
+            const c = data.days[a].schedule[i].time;//時間の取得
+            sunnyoutput2 += `<p class = 'sunnytime'>${c}</p><input id = ${sunnyatai} type="text" value="${inputValue}" class='input'><br>`; // 各アクティビティを`<p>`タグで囲んで出力
           }
         }
       }
+      // 結果をHTMLに表示
       resultElement.innerHTML = sunnyoutput2;
     });
   });
+  
