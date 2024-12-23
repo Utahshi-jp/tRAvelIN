8e5024938f7c6e06055313d0376d81ea883c29bc
sakuramotida <it232089@trident.ac.jp>
2024-12-23 15:34:28 +0900

    セレクトボックス追加

--- display_schedule/title.js
+++ display_schedule/title.js
@@ -5,7 +5,7 @@ fetch('sample.json')
     let output = ''; // 表示する文字列を格納する変数
     console.log(data);
     const b = data.title;//タイトルを格納
-    output += `<p><h2>${b}</h2></p>`; // 各アクティビティを`<p>`タグで囲んで出力
+    output += `<p><select><option id = "selectbox" value="option1">${b}</option></select></p>`; // 各アクティビティを`<p>`タグで囲んで出力
     console.log(output);
     resultElement.innerHTML = output; // 結果をHTMLに表示
   });
