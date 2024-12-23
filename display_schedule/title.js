295d59be0877b04d03f6cfde934aae0c0eceaf48
sakuramotida <it232089@trident.ac.jp>
2024-12-23 16:09:03 +0900

    タイトル画面セレクトボックス設置

--- display_schedule/title.js
+++ display_schedule/title.js
@@ -1,11 +1,30 @@
 fetch('sample.json')
   .then(response => response.json())
   .then(data => {
-    const resultElement = document.getElementById('title');//htmlと連携
-    let output = ''; // 表示する文字列を格納する変数
-    console.log(data);
+    const resultElement = document.getElementById('title');
+    let output = '';
     const b = data.title;//タイトルを格納
-    output += `<p><select><option id = "selectbox" value="option1">${b}</option></select></p>`; // 各アクティビティを`<p>`タグで囲んで出力
-    console.log(output);
-    resultElement.innerHTML = output; // 結果をHTMLに表示
+
+    // title 配列の要素をセレクトボックスのオプションとして追加
+    const titles = [b, "東京"];//配列としてタイトルを格納する
+    titles.forEach(title => {
+      output = `<p><select id="selectbox_${title}">`;//セレクトボックス作成
+      titles.forEach(option => {
+        output += `<option id="selectbox_${option}" value="${option}">${option}</option>`;//セレクトボックスの中身を作成
+      });
+      output += `</select></p>`;//閉じタグ
+    });
+
+    resultElement.innerHTML = output;//セレクトボックスをhtmlに反映
+
+     // すべてのセレクトボックスに対してイベントリスナーを追加
+    titles.forEach(option => {
+      const selectElement = document.getElementById(`selectbox_${option}`);
+      selectElement.addEventListener('change', (event) => {
+        const selectedElement = event.target;
+        const selectedValue = selectedElement.value;//セレクトボックスの中身を取得
+
+        console.log(`選択されたセレクトボックスの値は: ${selectedValue}`);//取得したものを表示
+      });
+    });
   });
